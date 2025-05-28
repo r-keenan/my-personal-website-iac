6e54d9063499e29a64ca8562687ec093350510fb
r-keenan <ross.keenan@protonmail.com>
2025-05-28 12:08:59 -0500

    Created a dynamodb table

--- Program.cs
+++ Program.cs
@@ -1,15 +1,107 @@
-﻿using Pulumi;
-using Pulumi.Aws.S3;
-using System.Collections.Generic;
+using Pulumi;
+using Pulumi.Aws.DynamoDB;
+using Pulumi.Aws.DynamoDB.Inputs;
 
 return await Deployment.RunAsync(() =>
 {
-    // Create an AWS resource (S3 Bucket)
-    var bucket = new BucketV2("my-bucket");
+    var contact_form_table = new Table("contact-form-table", new()
+    {
+        Name = "ContactMessages",
+        BillingMode = "PAY_PER_REQUEST", 
+        HashKey = "id",
+        RangeKey = "submittedAt",
+        Attributes = new[]
+        {
+            new TableAttributeArgs
+            {
+                Name = "id",
+                Type = "S", 
+            },
+            new TableAttributeArgs
+            {
+                Name = "submittedAt",
+                Type = "S", 
+            },
+            new TableAttributeArgs
+            {
+                Name = "email",
+                Type = "S", 
+            },
+            new TableAttributeArgs
+            {
+                Name = "subject",
+                Type = "S", 
+            },
+            new TableAttributeArgs
+            {
+                Name = "firstName",
+                Type = "S", 
+            },
+            new TableAttributeArgs
+            {
+                Name = "lastName",
+                Type = "S", 
+            },
+            new TableAttributeArgs
+            {
+                Name = "companyName",
+                Type = "S", 
+            },
+        },
+        GlobalSecondaryIndexes = new[]
+        {
+            new TableGlobalSecondaryIndexArgs
+            {
+                Name = "EmailIndex",
+                HashKey = "email",
+                RangeKey = "submittedAt",
+                ProjectionType = "ALL", 
+            },
+            new TableGlobalSecondaryIndexArgs
+            {
+                Name = "SubjectIndex",
+                HashKey = "subject",
+                RangeKey = "submittedAt",
+                ProjectionType = "ALL", 
+            },
+            new TableGlobalSecondaryIndexArgs
+            {
+                Name = "FirstNameIndex",
+                HashKey = "firstName",
+                RangeKey = "submittedAt",
+                ProjectionType = "ALL",
+            },
+            new TableGlobalSecondaryIndexArgs
+            {
+                Name = "LastNameIndex",
+                HashKey = "lastName",
+                RangeKey = "submittedAt",
+                ProjectionType = "ALL",
+            },
+            new TableGlobalSecondaryIndexArgs
+            {
+                Name = "CompanyIndex",
+                HashKey = "companyName",
+                RangeKey = "submittedAt",
+                ProjectionType = "ALL",
+            },
+        },
+        Ttl = new TableTtlArgs
+        {
+            AttributeName = "expiresAt",
+            Enabled = true,
+        },
+        Tags =
+        {
+            { "Name", "contact-form-table" },
+            { "Environment", "production" },
+            { "Purpose", "ContactFormSubmissions" },
+        },
+    });
 
-    // Export the name of the bucket
     return new Dictionary<string, object?>
     {
-        ["bucketName"] = bucket.Id
+        ["tableName"] = contact_form_table.Name,
+        ["tableArn"] = contact_form_table.Arn,
     };
 });
